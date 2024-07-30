3d5bb7e6efbdde459df99ab4bfa1772325f7d13a
ezdimitry <137773973+ezdimitry@users.noreply.github.com>
2024-07-30 15:25:04 +0300

    add sqlalchemy and visual cards for things

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import  Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from cloudipsp import Api, Checkout
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActive = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return self.title
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    items = Item.query.order_by(Item.price).all()
+    return render_template('index.html', data=items)
 
 
 @app.route("/about")
@@ -13,7 +29,25 @@ def about():
     return render_template('about.html')
 
 
+@app.route("/create", methods=["POST", "GET"])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+        item = Item(title=title, price=price)
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибка"
+    else:
+        return render_template('create.html')
+
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
 
